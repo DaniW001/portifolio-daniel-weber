321bde60b7cdf7bfb05ea7765a7807e5f2b5b278
Daniel <danielweber190306@gmail.com>
2026-05-08 20:55:19 -0300

    ajuste de botoes de acoes e inicio de estrutura de secoes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { animarTexto } from './animacaoTexto.js';
 import { inicializarMenu } from './menu.js';
-import { inicializarSlideshow } from './slideshow.js';
+import { inicializarSlideshow } from './slideShow.js';
 
 const textoBranco = document.querySelector('.digitando .texto-branco');
 const textoGradiente = document.querySelector('.digitando .texto-gradiente');
